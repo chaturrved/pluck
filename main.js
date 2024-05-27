411a2487afbf31bd318cc4b93ce86256b2ca16f9
chaturrved <chaturved.edu@gmail.com>
2024-05-27 17:25:44 +0530

    navbar and navmenu

---
+++ main.js
@@ -0,0 +1,11 @@
+const primaryHeader = document.querySelector(".primary-header");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+const primaryNav = document.querySelector(".primary-navigation");
+
+navToggle.addEventListener("click", () => {
+  primaryNav.toggleAttribute("data-visible");
+  primaryNav.hasAttribute("data-visible")
+    ? navToggle.setAttribute("aria-expanded", true)
+    : navToggle.setAttribute("aria-expanded", false);
+  primaryHeader.toggleAttribute("data-overlay");
+});
