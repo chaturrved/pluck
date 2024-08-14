76ee646b2761a47a101b52d0ab7825d99f6ee031
Chaturved Degloorkar <chaturved.edu@gmail.com>
2024-08-15 04:56:35 +0530

    update
    update

--- main.js
+++ main.js
@@ -3,9 +3,15 @@ const navToggle = document.querySelector(".mobile-nav-toggle");
 const primaryNav = document.querySelector(".primary-navigation");
 
 navToggle.addEventListener("click", () => {
+  const isVisible = primaryNav.hasAttribute("data-visible");
+
   primaryNav.toggleAttribute("data-visible");
-  primaryNav.hasAttribute("data-visible")
-    ? navToggle.setAttribute("aria-expanded", true)
-    : navToggle.setAttribute("aria-expanded", false);
+  navToggle.setAttribute("aria-expanded", isVisible ? "false" : "true");
   primaryHeader.toggleAttribute("data-overlay");
+
+  // Toggle between hamburger and close icons
+  const hamburgerIcon = navToggle.querySelector(".icon-hamburger");
+  const closeIcon = navToggle.querySelector(".icon-close");
+  hamburgerIcon.style.display = isVisible ? "block" : "none";
+  closeIcon.style.display = isVisible ? "none" : "block";
 });
